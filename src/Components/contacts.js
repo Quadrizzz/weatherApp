fddc9949f74c534dc47b11347b5c0211c662ff02
Quadrizz <mackquadrizz@gmail.com>
2020-11-11 03:17:05 +0100

    done

--- src/Components/contacts.js
+++ src/Components/contacts.js
@@ -4,9 +4,9 @@ import '../Container/App.css'
 const Contacts = ()=>{
     return(
         <div className = "mt4 condiv">
-            <h1 className = "tc">Email : mackquadrizz@gmai.com</h1>
+            <h1 className = "tc">Email : mackquadrizz@gmail.com</h1>
             <h1 className = "tc">Phone : +2348065811581</h1>
-            <h1 className = "tc">Twwitter : @IWriteICode</h1>
+            <h1 className = "tc">Twwitter : @Azur_xx</h1>
             <h1 className = "tc">Github : github.com/Quadrizzz</h1>
         </div>
     )
